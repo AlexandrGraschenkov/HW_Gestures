92e5564bacadbfcd3a8648aac0603f8d543e4370
Alexander <sharrikk@gmail.com>
2014-04-28 23:41:53 +0400

    init

---
+++ TestGesture/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  TestGesture
+//
+//  Created by Alexander on 28.04.14.
+//  Copyright (c) 2014 Alexander. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic, weak) IBOutlet UIImageView *imgView;
+@end
+
+@implementation ViewController
+@synthesize imgView;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // ниче нет, пусто!
+}
+
+@end
---
+++ TestGestureTests/TestGestureTests.m
@@ -0,0 +1,34 @@
+//
+//  TestGestureTests.m
+//  TestGestureTests
+//
+//  Created by Alexander on 28.04.14.
+//  Copyright (c) 2014 Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestGestureTests : XCTestCase
+
+@end
+
+@implementation TestGestureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
