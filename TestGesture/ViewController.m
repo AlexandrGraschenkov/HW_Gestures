96defa3a45ac2ed4f9bb93fded13defd1adda446
Amir Zigangarayev <av0kado@bk.ru>
2014-05-20 16:41:51 +0400

    +1

--- TestGesture/ViewController.m
+++ TestGesture/ViewController.m
@@ -9,7 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    CGFloat startHeight;
+    CGFloat sectionHeight;
+    int imageNumb;
+    int startNumb;
+}
 @property (nonatomic, weak) IBOutlet UIImageView *imgView;
+
 @end
 
 @implementation ViewController
@@ -18,8 +25,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // ниче нет, пусто!
+    imageNumb = 1;
+    imgView.image = [UIImage imageNamed:[NSString stringWithFormat:@"gesture%d", imageNumb]];
+    sectionHeight = self.view.frame.size.height / 20;
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    sectionHeight = self.view.frame.size.height / 20;
+}
+
+- (IBAction)swipe:(UIPanGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateBegan)
+    {
+        startHeight = [sender locationInView:self.view].y;
+        startNumb = imageNumb;
+    }
+    else if (sender.state == UIGestureRecognizerStateChanged)
+    {
+        int currentDelta = (int)(([sender locationInView:self.view].y - startHeight) / sectionHeight);
+        imageNumb = startNumb + currentDelta;
+        if (imageNumb < 1)
+        {
+            imageNumb = 1;
+        }
+        else if (imageNumb > 15)
+        {
+            imageNumb = 15;
+        }
+        if (currentDelta != 0)
+        {
+            imgView.image = [UIImage imageNamed:[NSString stringWithFormat:@"gesture%d", imageNumb]];
+        }
+    }
 }
 
 @end
